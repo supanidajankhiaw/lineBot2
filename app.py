4cfa3f1d14a3c951c1c305591cd1396840fc73df
supanidajankhiaw <51777339+supanidajankhiaw@users.noreply.github.com>
2019-09-19 18:53:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/3"
+    return "นางสาว สุภนิดา จันเขียว เลขที่28 ม.4/6"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
@@ -20,13 +20,13 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText(user,'เออดีด้วย')
-    elif (userText == 'ไอ้บ้า') :
-        sendText(user,'เอ็งซิบ้า')
+        sendText(user,'สวัสดี')
+    elif (userText == 'ทำอะไรอยู่') :
+        sendText(user,'กินข้าว')
     return '',200
 
 def sendText(user, text):
